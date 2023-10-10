114b3d654998b1832b6f9e74bcae776a108f2f79
kleyfiex <kleyfie92@gmail.com>
2023-10-10 15:06:44 +0300

    prv-version-site

--- script.js
+++ script.js
@@ -16,19 +16,24 @@ otherPlace.addEventListener("click", function () {
   window.open("https://vk.com/im?media=&sel=-222597982", "_blank");
 });
 
+const askBtn = document.getElementById("askBtn");
 
-
-const plusIcons = document.querySelectorAll(".icon-plus");
-
-plusIcons.forEach(function(plusIcon) {
-  plusIcon.addEventListener("click", function() {
-    const parentRow = plusIcon.parentElement;
-    const answer = parentRow.querySelector(".answer");
-
-    if (answer.style.display === "none" || answer.style.display === "") {
-      answer.style.display = "block";
-    } else {
-      answer.style.display = "none";
-    }
-  });
+askBtn.addEventListener("click", function () {
+  window.open("https://vk.com/topic-222597982_50343408", "_blank");
 });
+
+
+
+
+function toggleAnswer(icon) {
+    const answer = icon.closest('.row-question').querySelector('.answer');
+    const plusMinusIcon = icon.querySelector('.plus-minus');
+  
+    answer.classList.toggle('active');
+    if (answer.classList.contains('active')) {
+      plusMinusIcon.style.transform = 'rotate(45deg)'; // Изменяем угол поворота на 45 градусов для "минуса"
+    } else {
+      plusMinusIcon.style.transform = 'rotate(0deg)'; // Возвращаем угол поворота в исходное состояние для "плюса"
+    }
+  }
+  
